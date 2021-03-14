0114d3568155f61cb0591ce6afacc9eb946269cf
Hubert Sieczka <huberts94@gmail.com>
2021-03-14 22:06:37 +0100

    Formy website

--- test.py
+++
@@ -1,8 +0,0 @@
-from selenium import webdriver
-
-driver = webdriver.Chrome(executable_path=r'D:\Selenium-GRID\chromedriver.exe')
-driver.get('https://google.com')
-web_element = driver.find_element_by_name('q')
-web_element.send_keys('cheese!')
-web_element.submit()
-driver.quit() 
